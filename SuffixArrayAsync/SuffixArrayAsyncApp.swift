c6b6acdcc384c52e1781125e74c687246093e25f
Artem Bolotov <teeemon@gmail.com>
2023-03-03 08:04:22 +0300

    Initial Commit

---
+++ SuffixArrayAsync/SuffixArrayAsyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuffixArrayAsyncApp.swift
+//  SuffixArrayAsync
+//
+//  Created by artembolotov on 03.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct SuffixArrayAsyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
